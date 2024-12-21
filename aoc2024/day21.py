4d7c0b2eb37a0def38bdae6c250b96a81efd74ad
antho <anthony@nguyen.ly>
2024-12-21 19:19:41 +0000

    day 21

--- aoc2024/day21.py
+++ aoc2024/day21.py
@@ -1,48 +1,68 @@
-import networkx as nx
+from collections import namedtuple
+from functools import lru_cache
+from itertools import product
+class Point(namedtuple('Point',['x', 'y'])):
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y )
 
-NUMPAD = nx.DiGraph()
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
 
-N = -1j
-S = 1j
-W = -1
-E = 1
-D_TO_LBL = {N: '^', S: 'v', E: '>', W: '<'}
-LBL_TO_D = {v: k for k, v in D_TO_LBL.items()}
 
-FOURDIRS = [N, S, E, W]
-numpadstr = '''789
-456
-123
- 0A'''
+# Keypads
+NUMPAD = {k:Point(i%3, (3-i//3)) for i, k in enumerate("789456123 0A")}
+DIRPAD = {k:Point(i%3, (3-i//3)) for i, k in enumerate(" ^A<v>")}
 
-from utils import *
-co_to_c, _, _ = parse_complex(numpadstr)
+'''
+Returns only sequences that have repeating characters
+'''
+def best(start, end, keypad):
+    pt_a, pt_v, pt_space = keypad[start], keypad[end], keypad[' ']
+    pt_diff = pt_v - pt_a
 
-for co in co_to_c:
-    for d in FOURDIRS:
-        if co + d in co_to_c and co_to_c[co] != ' ' and co_to_c[co + d] != ' ':
-            NUMPAD.add_edge(co_to_c[co], co_to_c[co + d], label = D_TO_LBL[d])
+    toret = set()
 
-dirpadstr = ''' ^A
-<v>'''
+    v_seq = pt_diff.y * '^' + 'v' * - pt_diff.y
+    h_seq = pt_diff.x * '>' + '<' * - pt_diff.x
 
-from utils import *
-co_to_c, _, _ = parse_complex(dirpadstr)
-DIRPAD = nx.DiGraph()
-for co in co_to_c:
-    for d in FOURDIRS:
-        if co + d in co_to_c and co_to_c[co] != ' ' and co_to_c[co + d] != ' ':
-            DIRPAD.add_edge(co_to_c[co], co_to_c[co + d], label = D_TO_LBL[d])
+    # if we end up on space don't add the step
+    if pt_a + Point(0, pt_diff.y) != pt_space:
+        toret.add(v_seq + h_seq + "A")
+    if pt_a + Point(pt_diff.x, 0) != pt_space:
+        toret.add(h_seq + v_seq + "A")
+    return toret
 
-code = '029A'
-def get_presses(code, pad):
-    spath = [nx.shortest_path(pad, x1, x2)  for x1, x2 in zip('A' + code, code) ]
-    presses_without_A = [[pad.edges[x1, x2]['label'] for x1, x2 in zip(p, p[1:])] for p in spath]
-    presses = 'A'.join([''.join(x) for x in presses_without_A]) + 'A'
-    return presses
-numpad1 = get_presses(code, NUMPAD)
-dirpad1 = get_presses(numpad1, DIRPAD)
-dirpad2 = get_presses(dirpad1, DIRPAD)
-dirpad3 = get_presses(dirpad2, DIRPAD)
-len(dirpad3) * int( ''.join([c for c in code if c != 'A' and c != '0']) ) #
-pass
+'''
+Get a list of moves for a combination and a certain keypad
+for example c = '805A' and keypad = NUMPAD
+Assumption is that we start on 'A' 
+'''
+def moves(c, keypad):
+    return {"".join(x) for x in product(*[best(*p, keypad)
+                                          for p in zip("A"+c, c)])}
+
+@lru_cache(maxsize=None)
+def count(m, depth):
+    if depth == 0: return len(m)
+    return sum( min(count(s, depth-1) for s in moves(part+"A", DIRPAD))
+              for part in m[:-1].split("A"))
+
+
+def part1(inp):
+    total = 0
+    depth = 2
+    for code in inp.splitlines():
+        total += int(code[:-1]) * min(count(move, depth) for move in moves(code, NUMPAD))
+    return total
+
+def part2(inp):
+    total = 0
+    depth = 25
+    for code in inp.splitlines():
+        total += int(code[:-1]) * min(count(move, depth) for move in moves(code, NUMPAD))
+    return total
+
+from aoc_lube import fetch
+inp = fetch(2024, 21)
+print(part1(inp))
+print(part2(inp))
