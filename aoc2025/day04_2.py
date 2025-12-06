47c82c096a5ebf93d5ce924fa04b6cc57b1ce4bb
anthonynguyen <anthony@nguyen.ly>
2025-12-06 12:45:20 +0000

    convolve version of 2024/day4

--- aoc2025/day04_2.py
+++ aoc2025/day04_2.py
@@ -8,17 +8,32 @@ from scipy.ndimage import convolve
 
 s = fetch(2025, 4)
 
-s = '''..@@.@@@@.
-@@@.@.@.@@
-@@@@@.@.@@
-@.@@@@..@.
-@@.@@@@.@@
-.@@@@@@@.@
-.@.@.@.@@@
-@.@@@.@@@@
-.@@@@@@@@.
-@.@.@@@.@.'''
+# s = '''..@@.@@@@.
+# @@@.@.@.@@
+# @@@@@.@.@@
+# @.@@@@..@.
+# @@.@@@@.@@
+# .@@@@@@@.@
+# .@.@.@.@@@
+# @.@@@.@@@@
+# .@@@@@@@@.
+# @.@.@@@.@.'''
 
-rolls = np.array([list(map('@'.__eq__, line)) for line in s.split('\n')], dtype=bool)
+grid = np.array([list(map('@'.__eq__, line)) for line in s.split('\n')], dtype=bool)
+rolls = grid*1
 
-convolve(rolls*1, np.ones((3,3), dtype=int), mode='constant') & rolls
+neighbours = convolve(rolls*1, np.ones((3,3), dtype=int), mode='constant')
+removed = (neighbours < 5) & rolls
+print(f"part 1: {removed}")
+
+part2 = 0
+while True:
+    part2 += np.sum(removed)
+    rolls = rolls & (neighbours >= 5)
+    neighbours = convolve(rolls*1, np.ones((3,3), dtype=int), mode='constant')
+    removed = (neighbours < 5) & rolls
+    if np.sum(removed) == 0:
+        break
+
+print(f"part 2: {part2}")
+
