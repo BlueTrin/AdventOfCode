af216b6f903edfac4f5159b673a6d6ba71c8ff28
anthonynguyen <anthony@nguyen.ly>
2025-12-06 12:47:38 +0000

    added comments

--- aoc2025/day04_2.py
+++ aoc2025/day04_2.py
@@ -22,6 +22,7 @@ s = fetch(2025, 4)
 grid = np.array([list(map('@'.__eq__, line)) for line in s.split('\n')], dtype=bool)
 rolls = grid*1
 
+# Any cell which is a roll and has less than 5 rolls in its 3x3 neighbourhood (including itself) is removed
 neighbours = convolve(rolls*1, np.ones((3,3), dtype=int), mode='constant')
 removed = (neighbours < 5) & rolls
 print(f"part 1: {removed}")
@@ -29,10 +30,13 @@ print(f"part 1: {removed}")
 part2 = 0
 while True:
     part2 += np.sum(removed)
+
+    # we need to remove the rolls which had less than 5 neighbours (including itself)
     rolls = rolls & (neighbours >= 5)
     neighbours = convolve(rolls*1, np.ones((3,3), dtype=int), mode='constant')
     removed = (neighbours < 5) & rolls
     if np.sum(removed) == 0:
+        # if we could not remove anymore, we are done
         break
 
 print(f"part 2: {part2}")
